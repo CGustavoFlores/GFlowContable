8d80f76b6db2875f161779ea080cb15eabadde4c
CGustavoFlores <80056477+CGustavoFlores@users.noreply.github.com>
2022-10-26 02:41:21 -0300

    con base datos postgresql

--- PryGFlow/settings.py
+++ PryGFlow/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = "PryGFlow.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "gflow",
+        "USER": 'postgres',
+        "PASSWORD": "gflow123",
+        "HOST" : "127.0.0.1",
+        "DATABASE_PORT" : "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
